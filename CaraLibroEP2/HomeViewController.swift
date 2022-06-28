16745ab7026d7804791b436b3e806af286ac3b7c
Aavz321 <ado.avz@gmail.com>
2022-06-28 13:33:44 -0700

    correciónn

--- CaraLibroEP2/HomeViewController.swift
+++ CaraLibroEP2/HomeViewController.swift
@@ -5,4 +5,28 @@
 //  Created by Adolfo on 28/06/22.
 //
 
-import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+            
+        
+    }
+
+    
+
+}
+
+extension HomeViewController{
+    class func build() -> HomeViewController {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController
+        return controller ?? HomeViewController()
+    }
+}
+
--- CaraLibroEP2/LoginViewController.swift
+++ CaraLibroEP2/LoginViewController.swift
@@ -6,7 +6,35 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
 
+    @IBAction func LoginButtonAction(_ sender: Any) {
+        if let email = emailTextField.text, let password =
+            passwordTextField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) {
+                (result, error) in
+                
+                if let result = result, error == nil{
+                    let controller = HomeViewController()
+                    self.navigationController?
+                        .pushViewController(controller, animated: true)
+                }else{
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando el usuario", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                
+            }
+        }
+    }
+    
+    
 }
