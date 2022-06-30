4ae3f3d9cea9dcfc5d3a5f9f9cd381d385bde02c
Aavz321 <ado.avz@gmail.com>
2022-06-30 10:49:54 -0700

    recuperar contraseña

---
+++ CaraLibroEP2/RecuperarViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RecuperarViewController.swift
+//  CaraLibroEP2
+//
+//  Created by Adolfo on 29/06/22.
+//
+
+import UIKit
+import Firebase
+
+class RecuperarViewController: UIViewController{
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func forgotPassButton_Tapped(_ sender: Any){
+        let auth = Auth.auth()
+        
+        auth.sendPasswordReset(withEmail: emailTextField.text!) { (error) in
+            if let error = error {
+                let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+            
+                self.present(alertController, animated: true, completion: nil)
+            }
+            let alertController = UIAlertController(title: "Correo enviado", message: "El correo de verificación se envió correctamente", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+        
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+}
